7ce21ac219ff28dceb317b48efca808d8930297a
lajmi69 <lajmi591@gmail.com>
2024-10-02 12:53:25 +0100

    version2

--- app/Models/Scategorie.php
+++ app/Models/Scategorie.php
@@ -12,6 +12,10 @@ class Scategorie extends Model
     public function categorie(){
         return $this->belongsTo(Categorie::class,"categorieID");
     }
+    public function articles()
+{
+return $this->hasMany(Article::class,"scategorieID");
+}
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\ScategorieController;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -19,4 +20,8 @@ Route::delete('/categories/{id}', [CategorieController::class, "destroy"]);*/
 Route::middleware('api')->group(function () {
     Route::resource('scategories', ScategorieController::class);
     });
-
+    Route::middleware('api')->group(function () {
+        Route::resource('articles', ArticleController::class);
+        });
+        Route::get('/listarticles/{idscat}', [ArticleController::class,'showArticlesBySCAT']);
+        Route::get('/articles/art/articlespaginate', [ArticleController::class,'articlesPaginate']);
